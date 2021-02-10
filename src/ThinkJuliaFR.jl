c1b2f3fb33a475646ed9e1d93ac2fada681bdb58
aquarelleX332 <thierry.lepoint@protonmail.com>
2021-02-10 18:43:14 +0100

    modification_1

--- src/ThinkJuliaFR.jl
+++ src/ThinkJuliaFR.jl
@@ -12,21 +12,21 @@ module ThinkJuliaFR
   
   const depsjl_path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
   if !isfile(depsjl_path)
-      error("IntroAJulia not installed properly, run Pkg.build(\"IntroAJulia\"), restart Julia and try again")
+      error("ThinkJuliaFR not installed properly, run Pkg.build(\"ThinkJuliaFR\"), restart Julia and try again")
   end
   include(depsjl_path)
-
+"""
   function __init__()
     # Always check your dependencies that live in `deps.jl`
     check_deps()
     global setcolor! = Core.eval(Base, :(x -> (y = have_color; global have_color = x; y)))
     CAN_INLINE[] = Base.JLOptions().can_inline == 0 ? false : true
   end
-
+"""
   #import Base.+
 
   export printlyrics, repeatlyrics, printtwice, cattwice
-  export rightjustify, imprimircuadricula
+  export rightjustify
   export Turtle, forward, turn, penup, pendown, @svg
   #export countdown, printn, recurse
   #export area, absvalue, distance, circlearea, isdivisible, fact, fib
